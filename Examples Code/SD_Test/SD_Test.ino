53aa0d39d9589084936eda90812774d0f8458d54
Banpot <banpot408@gmail.com>
2020-09-28 00:01:32 +0700

    move i2c code to excample and edit code sd card test

--- Examples Code/SD_Test/SD_Test.ino
+++ Examples Code/SD_Test/SD_Test.ino
@@ -13,7 +13,7 @@ while (!Serial) {
 
 
 Serial.print("Initializing SD card...");
-pinMode(SS, OUTPUT);
+pinMode(chipSelect, OUTPUT);
 
 if (!SD.begin(chipSelect)) {
 Serial.println("initialization failed!");
@@ -21,23 +21,23 @@ return;
 }
 Serial.println("initialization done.");
 
-myFile = SD.open("test.txt", FILE_WRITE); // เปิดไฟล์ที่ชื่อ test.txt เพื่อเขียนข้อมูล โหมด FILE_WRITE
+myFile = SD.open("bes.txt", FILE_WRITE); // เปิดไฟล์ที่ชื่อ test.txt เพื่อเขียนข้อมูล โหมด FILE_WRITE
 
 // ถ้าเปิดไฟล์สำเร็จ ให้เขียนข้อมูลเพิ่มลงไป
 if (myFile) {
-Serial.print("Writing to test.txt...");
+Serial.print("Writing to bes.txt...");
 myFile.println("Designed by BanpotPCBDesign Tel: 062 482 0181, Line: banpot_ya"); // สั่งให้เขียนข้อมูล
 myFile.close(); // ปิดไฟล์
 Serial.println("done.");
 } else {
 // ถ้าเปิดไฟลืไม่สำเร็จ ให้แสดง error 
-Serial.println("error opening test.txt");
+Serial.println("error opening bes.txt");
 }
 
 // เปิดไฟล์เพื่ออ่าน
-myFile = SD.open("test.txt"); // สั่งให้เปิดไฟล์ชื่อ test.txt เพื่ออ่านข้อมูล
+myFile = SD.open("bes.txt"); // สั่งให้เปิดไฟล์ชื่อ test.txt เพื่ออ่านข้อมูล
 if (myFile) {
-Serial.println("test.txt:");
+Serial.println("bes.txt:");
 // อ่านข้อมูลทั้งหมดออกมา
 while (myFile.available()) {
 Serial.write(myFile.read());
@@ -45,7 +45,7 @@ Serial.write(myFile.read());
 myFile.close(); // เมื่ออ่านเสร็จ ปิดไฟล์
 } else {
 // ถ้าอ่านไม่สำเร็จ ให้แสดง error 
-Serial.println("error opening test.txt");
+Serial.println("error opening bes.txt");
 }
 }
 
